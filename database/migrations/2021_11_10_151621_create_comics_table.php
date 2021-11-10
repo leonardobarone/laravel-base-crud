8e15772eb4ecce0ddfa660f332fe1ed28fbebcc0
leonardobarone <leonardobarone26@gmail.com>
2021-11-10 17:19:29 +0100

    first commit

---
+++ database/migrations/2021_11_10_151621_create_comics_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 60);
+            $table->text('description');
+            $table->string('thumb', 255)->nullable();
+            $table->float('price', 5, 2)->unsigned();
+            $table->string('series', 60);
+            $table->string('type', 60);
+            $table->date('sale_date', 60);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+}
